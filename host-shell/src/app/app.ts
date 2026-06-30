971823dfa60dda0fd594dfc7fbfdfa6ec326d041
Gunjan Kansara <gunjkansara@gmail.com>
2026-06-30 20:53:37 +0530

    chore: update app bootstrap configuration

--- host-shell/src/app/app.ts
+++ host-shell/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('host-shell');
+  protected readonly title = signal('host-shell app');
 }
